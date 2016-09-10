7b980dff694af09a4ba5e323b96d036d6c67af21
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-09-10 20:30:08 +0100

    WIP get current user

--- src/Api.elm
+++ src/Api.elm
@@ -1,4 +1,4 @@
-module Api exposing (tokenUrl, fbAuthUrl, Msg(..), authenticateCmd, postJson)
+module Api exposing (tokenUrl, fbAuthUrl, Msg(..), UserInfo, authenticateCmd, getMeCmd)
 
 import Http
 import Task exposing (Task)
@@ -12,6 +12,9 @@ apiRoot = "http://localhost:4000"
 tokenUrl : String
 tokenUrl = apiRoot ++ "/token"
 
+userInfoUrl : String
+userInfoUrl = apiRoot ++ "/me"
+
 -- TODO: move client_id and redirect_uri into environment variables
 fbAuthUrl : String
 fbAuthUrl = "https://www.facebook.com/dialog/oauth?client_id=1583083701926004&redirect_uri=http://localhost:3000/"
@@ -22,6 +25,7 @@ fbAuthUrl = "https://www.facebook.com/dialog/oauth?client_id=1583083701926004&re
 type Msg
   = GotAccessToken String
   | AuthFailed Http.Error
+  | GotUserInfo UserInfo
 
 
 accessTokenDecoder : Decoder String
@@ -42,6 +46,22 @@ authenticateCmd wrapFn authCode =
       |> Cmd.map wrapFn
 
 
+type alias UserInfo =
+  { id : String
+  }
+
+userInfoDecoder : Decoder UserInfo
+userInfoDecoder =
+  Decode.object1 UserInfo
+    (Decode.at ["data", "id"] Decode.string)
+
+getMeCmd : (Msg -> a) -> String -> Cmd a
+getMeCmd wrapFn accessToken =
+  getJsonWithAuth accessToken userInfoDecoder userInfoUrl
+    |> Task.perform AuthFailed GotUserInfo
+    |> Cmd.map wrapFn
+
+
 postJsonWithHeaders : List (String, String) -> Decoder a -> String -> String -> Task Http.Error a
 postJsonWithHeaders headers responseDecoder url jsonBody =
   { verb = "POST", headers = [("Content-Type", "application/json")] ++ headers, url = url, body = Http.string jsonBody }
@@ -51,3 +71,17 @@ postJsonWithHeaders headers responseDecoder url jsonBody =
 postJson : Decoder a -> String -> String -> Task Http.Error a
 postJson =
   postJsonWithHeaders []
+
+
+getJsonWithAuth : String -> Decoder a -> String -> Task Http.Error a
+getJsonWithAuth accessToken responseDecoder url =
+  { verb = "GET"
+  , headers =
+      [ ("Authorization", "Bearer " ++ accessToken)
+      , ("Content-Type", "application/vnd.api+json")
+      ]
+  , url = url
+  , body = Http.empty
+  }
+  |> Http.send Http.defaultSettings
+  |> Http.fromJson responseDecoder
