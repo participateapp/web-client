30c9ba4163e16ebdd187f27a42efa85bc397ff2e
Thomas Weiser <git@thomasweiser.de>
2016-11-07 19:54:17 +0100

    Use modd and devd instead of npm and webpack
    Webpack is currently only used for automatically compiling the elm sources and reloading in the browser.
    
    The tools modd/devd do the same with much less boilerplate. No need to have the npm machinery.
    
    https://github.com/cortesi/modd
    https://github.com/cortesi/devd
    
    Current version v0.6 of devd doesn't support serving SPAs with non-hash-routes.
    See this related issue: https://github.com/cortesi/devd/issues/38
    
    Since we don't want to use hash-routes, we will not use this modd/devd setup as long as devd doesn't support them.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Require index.html so it gets copied to dist
-require('./index.html');
-
-var Elm = require('./Main.elm');
 var mountNode = document.getElementById('main');
 
 var programFlags = {
