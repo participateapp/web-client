28921a2adfe856c438ffacd4cae626341b4f4ddb
Thomas Weiser <git@thomasweiser.de>
2016-12-12 22:18:04 +0100

    Integrate css and image files from their directories

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,10 @@ module.exports = {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
         loader: 'file-loader',
       },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader'
+      }
     ],
 
     noParse: /\.elm$/,
