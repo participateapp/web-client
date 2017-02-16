53537c382e40a2e32a12dbd34c5bb44a761cbf72
Thomas Weiser <github@thomasweiser.de>
2017-02-16 18:11:28 +0100

    Move configuration values into new Config.elm (#62)
    * Use backend API at participate-api.herokuapp.com
    
    * Set facebook redirect app to gh-pages URL
    
    ... at https://oliverbarnes.github.io/participate/facebook_redirect
    
    * Set all URL resources relative to configurable basePath
    
    * index.html: Load JS file with relative path
    
    * Extract configurable settings into new module Config.elm
    
    * Configure for local testing
    
    * Provide two configs (local testing / heroku)
    
    * Fix config of basePath of location of gh-pages
    
    After moving and renaming the GitHub repo the new path to the app is now:
        https://participateapp.github.io/web-client
    
    * Deploy web-client to root dir
    
    * When deploying to gh-pages, we need an absolute path for resource.
    
    Reason:
    - gh-pages shall serve the same html file for any initial route (routes without a hash sign).
    - Therefore we link a 404.html to index.html. That's quite a hack but seem to work.
    - Running app thinks that it is served from that route, e.g. "/proposals/1".
    - To be independent from current location, all links should be based on absolute paths.

---
+++ src/Config_heroku.elm
@@ -0,0 +1,21 @@
+module Config exposing (..)
+
+
+baseRoot =
+    "https://participateapp.github.io"
+
+
+basePath =
+    ""
+
+
+facebookRedirectPath =
+    "facebook_redirect"
+
+
+facebookClientId =
+    "1583083548592686"
+
+
+apiUrl =
+    "https://participate-api.herokuapp.com"
